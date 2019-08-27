a135eebed374b53a47d8b2c6edeeea54d2627443
hampus.mann <hampus.mann@elev.ga.ntig.se>
2019-08-27 10:59:40 +0200

    Arraylist och posts

--- src/loggbok.java
+++ src/loggbok.java
@@ -1,10 +1,11 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class loggbok {
     public static void main(String[] args) {
         Scanner tgb = new Scanner(System.in);
-
-
+        ArrayList<String> posts = new ArrayList<String>();
+       
         System.out.println(
                 "1, visa \n" +
                 "2, lägg till post \n" +
@@ -16,8 +17,11 @@ public class loggbok {
         System.out.println("Vad vill du göra?");
         int choice = tgb.nextInt();
 
-        if (choice == 1) {
-            
+        if (choice == 2) {
+            System.out.println("Vad ska loggen heta?");
+            String name = tgb.nextLine();
+            System.out.println("Innehål");
+            String info = tgb.nextLine();
         }
     }
 }
