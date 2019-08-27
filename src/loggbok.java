8c4cf6c347de1c403ca1f5608e4af52eaaa18825
hampus.mann <hampus.mann@elev.ga.ntig.se>
2019-08-27 10:31:30 +0200

    visa klar

--- src/loggbok.java
+++ src/loggbok.java
@@ -1,2 +1,10 @@
 public class loggbok {
+    public static void main(String[] args) {
+        System.out.println("1, visa \n" +
+                "2, lägg till post \n" +
+                "3, uppdatera \n" +
+                "4, spara \n" +
+                "5, läs in \n" +
+                "6, avsluta" );
+    }
 }
