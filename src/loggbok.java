dc0f0a96bab095ed38125b2a0cd3c1c67bf2223a
hampus.mann <hampus.mann@elev.ga.ntig.se>
2019-08-27 11:06:14 +0200

    Man kan nu göra posts

--- src/loggbok.java
+++ src/loggbok.java
@@ -5,7 +5,8 @@ public class loggbok {
     public static void main(String[] args) {
         Scanner tgb = new Scanner(System.in);
         ArrayList<String> posts = new ArrayList<String>();
-       
+
+        System.out.println("Vad vill du göra?");
         System.out.println(
                 "1, visa \n" +
                 "2, lägg till post \n" +
@@ -14,14 +15,17 @@ public class loggbok {
                 "5, läs in \n" +
                 "6, avsluta" );
 
-        System.out.println("Vad vill du göra?");
         int choice = tgb.nextInt();
+        tgb.nextLine();
 
         if (choice == 2) {
             System.out.println("Vad ska loggen heta?");
             String name = tgb.nextLine();
             System.out.println("Innehål");
             String info = tgb.nextLine();
+            String post = name + "\n\n" + info;
+            posts.add(post);
+            System.out.println(posts);
         }
     }
 }
