6cc2c9ae9bd979c76f318e47b1c3e152f34b5557
hampus.mann <hampus.mann@elev.ga.ntig.se>
2019-08-27 10:41:44 +0200

    visa klar

--- src/loggbok.java
+++ src/loggbok.java
@@ -1,10 +1,23 @@
+import java.util.Scanner;
+
 public class loggbok {
     public static void main(String[] args) {
-        System.out.println("1, visa \n" +
+        Scanner tgb = new Scanner(System.in);
+
+
+        System.out.println(
+                "1, visa \n" +
                 "2, lägg till post \n" +
                 "3, uppdatera \n" +
                 "4, spara \n" +
                 "5, läs in \n" +
                 "6, avsluta" );
+
+        System.out.println("Vad vill du göra?");
+        int choice = tgb.nextInt();
+
+        if (choice == 1) {
+            
+        }
     }
 }
